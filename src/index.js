49b5be4d6e01da14189043492236bb2704076a3c
zhoujie <zhoujiejay@didichuxing.com>
2017-08-29 16:53:13 +0800

    remove logger since we have redux plugin

--- src/index.js
+++ src/index.js
@@ -7,23 +7,13 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk';
 import root_reducer from './root_reducer'
-import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
-import CategoryView from './components/CategoryView'
+import {BrowserRouter} from 'react-router-dom'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd(action.type)
-  return result
-}
-
 const store = createStore(root_reducer,
   composeEnhancers(
-    applyMiddleware(logger, thunk)
+    applyMiddleware(thunk)
   ))
 
 ReactDOM.render(
