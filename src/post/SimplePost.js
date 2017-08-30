bbbe85b449c1f5397e123540b4cf5464da7fb830
zhoujie <zhoujiejay@didichuxing.com>
2017-08-30 10:46:55 +0800

    use class to replace function component

--- src/post/SimplePost.js
+++ src/post/SimplePost.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 import * as actions from '../post/actions'
 import { filterDeletedPostComments } from '../utils/util'
 
-function SimplePost({post, comments, fromList, dispatch, history, deletePost, votePost}) {
-  const postComments = filterDeletedPostComments(post.id, comments)
-  const commentNumber = postComments.length
-  return (
-    <div>
-      <p>{'Title: ' + post.title}</p>
-      { !fromList && (<p>Body: {post.body}</p>)}
-      <p>{'timestamp: ' + new Date(post.timestamp).toLocaleString()}</p>
-      <p>{'Vote score: ' + post.voteScore}</p>
-      <p>{'Author: ' + post.author}</p>
-      <p>{'comments number: ' + commentNumber}</p>
-      <div className='inner'>
-        <Link to={`/post/${post.id}/edit`}><button className='btn-edit'></button></Link>
-        <button className='btn-delete' onClick={()=>deletePost(post.id, fromList?undefined: history)}></button>
-        <button className='btn-vote-up' onClick={()=>votePost(post.id, true)}></button>
-        <button className='btn-vote-down' onClick={()=>votePost(post.id, false)}></button>
+class SimplePost extends Component {
+
+  render() {
+    const {post, comments, fromList, dispatch, history, deletePost, votePost, getComments} = this.props
+    const postComments = filterDeletedPostComments(post.id, comments)
+    const commentNumber = postComments.length
+    return (
+      <div>
+        <p>{'Title: ' + post.title}</p>
+        { !fromList && (<p>Body: {post.body}</p>)}
+        <p>{'timestamp: ' + new Date(post.timestamp).toLocaleString()}</p>
+        <p>{'Vote score: ' + post.voteScore}</p>
+        <p>{'Author: ' + post.author}</p>
+        <p>{'comments number: ' + commentNumber}</p>
+        <div className='inner'>
+          <Link to={`/post/${post.id}/edit`}><button className='btn-edit'></button></Link>
+          <button className='btn-delete' onClick={()=>deletePost(post.id, fromList?undefined: history)}></button>
+          <button className='btn-vote-up' onClick={()=>votePost(post.id, true)}></button>
+          <button className='btn-vote-down' onClick={()=>votePost(post.id, false)}></button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 function mapStateToProps(state) {
